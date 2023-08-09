810a49548f95d8d7389377396abc830457cb16e7
PatricioSant <pasantibanez.dwfsc5@bootcampudd.cl>
2023-08-09 12:03:57 -0400

    Styles CSS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const $grafica = document.querySelector("#grafica");
 const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"];
 const datosVentas2020 = {
-  label: "Ventas por mes",
+  label: "Ventas 2023",
   data: [5000, 1500, 8000, 5102],
   backgroundColor: "rgba(54, 162, 235, 0.2)",
   borderColor: "rgba(54, 162, 235, 1)",
